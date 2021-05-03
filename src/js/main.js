2deb4342ac3f2b6937b4649fbab2d848e1c10ad9
thousand21 <thousand24@outlook.fr>
2021-05-03 15:30:14 +0200

    javascr

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+//exo1
+let tabExo=["van",,8,4,false]
+console.log(tabExo);
+
+//exo2
+console.log(tabExo.length);
+
+//exo3
+tabExo.pop();
+console.log(tabExo);
+//exo4
+tabExo.shift();
+console.log(tabExo);
+//exxo5
+tabExo.splice(tabExo.length,2,"papa","mama");
+console.log(tabExo);
+//exxo6
+tabExo.unshift(1,2,3);
+console.log(tabExo);
+//exo7
+
+console.log(tabExo.reverse());
+//exo8
+let boite=[];
+boite.push(1);
+boite.push(2);
+alert(boite);
+boite.unshift(3);
+alert(boite);
+boite.push(4);
+alert(boite);
+boite.pop();
+boite.pop();
+boite.shift();
+boite.shift();
+alert(boite);
+
